f9d2eaee5ca908633fac3bab5eb955a4080dae6a
Richert Lee RDMR-WDG-S <richertlr@wd-halen-lnx.ds.amrdec.army.mil>
2019-08-02 16:56:43 -0500

    I think there's been quite a few changes since my last commit. Most recently, I altered the initialization, so that DD^H is expected to be norm-preserving. I also removed some constant factors in initialization.

--- z_update_layer.py
+++ z_update_layer.py
@@ -5,22 +5,30 @@ import numpy as np
 import tensorflow.keras.constraints
 
 def floor_ceil(value):
+  # returns list of int: [floor(value),ceil(value)]
   return [int(np.floor(value)),int(np.ceil(value))]
 
-def cropped_l2_loss(x,xRecon):
+def evenly_cropped(x,xRecon):
+  #crops xRecon to the shape of x, trimming evenly from top and bottom, left and right. (In case of odd crop, top and left cropped less than bottom and right).
   inputShape = x.get_shape().as_list()
   reconShape = xRecon.get_shape().as_list()
   cropShape = (floor_ceil(np.float32(reconShape[1] - inputShape[1])/2.0),floor_ceil(np.float32(reconShape[2] - inputShape[2])/2.0))
-  croppedRecon = tf.keras.layers.Cropping2D(cropping=cropShape)(xRecon)
+  return tf.keras.layers.Cropping2D(cropping=cropShape)(xRecon)
+
+def cropped_l2_loss(x,xRecon):
+  # computes l2 loss between x and its reconstruction after cropping reconstruction to the size of x.
+  croppedRecon = evenly_cropped(x,xRecon)
   return tensorflow.nn.l2_loss(croppedRecon)
 
 def get_effective_filter_size(kerSz,poolSz,nol):
+  # Can only handle 1-dimension. Please loop and call multiple times if multiple dimensions are necessary.
   filterSz = 1
   for ii in range(nol-1,0,-1):
     filterSz = (filterSz + kerSz[ii] - 1)*poolSz[ii - 1]
   return filterSz + kerSz[0] - 1
 
 def get_padding_size(inputSz,kerSz,poolSz,nol):
+  # computes how much to pad the input and the size of the last layer. Can only handle one dimension.
   effFilterSz = get_effective_filter_size(kerSz,poolSz,nol)
   paddingSz = 2*effFilterSz - 2
   currSz = inputSz + paddingSz - kerSz[0] + 1
@@ -41,6 +49,20 @@ def get_padding_size(inputSz,kerSz,poolSz,nol):
     #endif
   #endfor
   return (paddingSz,currSz)
+
+def concat_splits(value,numOfSplits,splitDim,concatDim):
+  return tf.concat(tf.split(value,numOfSplits,splitDim),concatDim)
+
+def get_gathered_blocks(x,numOfBlocks)
+  gatheredRows = concat_splits(value=x,numOfBlocks[0],splitDim=1,concatDim=3)
+  return concat_splits(value=gatheredRows,numOfBlocks[1],splitDim=2,concatDim=3)
+
+def ungather_blocks(x,numOfBlocks,blockShape)
+    outputRows = concat_splits(value=x,numOfSplits=numOfBlocks[1],splitDim=3,concatDim=2)
+    return concat_splits(value=outputRows,numOfSplits=numOfBlocks[0],splitDim=3,concatDim=1)
+
+
+
 
 class shrinkage_layer(Layer):
 
@@ -122,24 +144,28 @@ class Unpool_LS(Layer):
     #   4) Find the maximum M from the values computed in step 3. Get the corresponding index maxInd.
     #   5) Change certain elements to M (unsortedNew = unsorted, unsortedNew[inds[0:maxInd + 1]] = M)
 
-    concat_splits = lambda value,numOfSplits,splitDim,concatDim: tf.concat(tf.split(value,numOfSplits,splitDim),concatDim)
-
     pooledShape = x[1].get_shape().as_list()
 
 
     # vectorize blocks
-    gatheredRows = concat_splits(value=x[0],numOfSplits=pooledShape[1],splitDim=1,concatDim=3)
-    gatheredRows2 = concat_splits(value=x[1],numOfSplits=pooledShape[1],splitDim=1,concatDim=3)
-    gatheredBlocks = concat_splits(value=gatheredRows,numOfSplits=pooledShape[2],splitDim=2,concatDim=3)
-    gatheredBlocks2 = concat_splits(value=gatheredRows2,numOfSplits=pooledShape[2],splitDim=2,concatDim=3)
+    gatheredBlocks = get_gathered_blocks(x[0],(pooledShape[1],pooledShape[2]))
     blockShape = gatheredBlocks.get_shape().as_list()
     blockSize = blockShape[1]*blockShape[2]
     vectorizedBlocks = tf.reshape(gatheredBlocks,[-1,blockSize,1,blockShape[3]])
+    vectorizedBlocks2 = get_gathered_blocks(x[1],(pooledShape[1],pooledShape[2]))
+    
+#    gatheredRows = concat_splits(value=x[0],numOfSplits=pooledShape[1],splitDim=1,concatDim=3)
+#    gatheredRows2 = concat_splits(value=x[1],numOfSplits=pooledShape[1],splitDim=1,concatDim=3)
+#    gatheredBlocks = concat_splits(value=gatheredRows,numOfSplits=pooledShape[2],splitDim=2,concatDim=3)
+#    gatheredBlocks2 = concat_splits(value=gatheredRows2,numOfSplits=pooledShape[2],splitDim=2,concatDim=3)
+#    blockShape = gatheredBlocks.get_shape().as_list()
+#    blockSize = blockShape[1]*blockShape[2]
+#    vectorizedBlocks = tf.reshape(gatheredBlocks,[-1,blockSize,1,blockShape[3]])
 
     # compute replacement values in least-squares solution and identify which elements to replace
     inds = tf.argsort(values=vectorizedBlocks,axis=1,direction='DESCENDING')
     sortedblk = tf.sort(values=vectorizedBlocks,axis=1,direction='DESCENDING')
-    cumSum = tf.cumsum(x=sortedblk,axis=1) + gatheredBlocks2
+    cumSum = tf.cumsum(x=sortedblk,axis=1) + vectorizedBlocks2
     cumAvg = tf.multiply(cumSum,tf.constant(np.reciprocal(np.arange(2.0,blockSize + 2.0)),dtype=tf.float32,shape=(1,blockSize,1,1)))
     replacementVals = tf.math.reduce_max(cumAvg,axis=1,keepdims=True)
     argMax = tf.reshape(tf.math.argmax(cumAvg,axis=1),[-1,1,1,blockShape[3]])
@@ -161,12 +187,15 @@ class Unpool_LS(Layer):
     # undo vectorization of blocks
     outputBlocks = tf.reshape(vectorizedOutput,[batchSize,*blockShape[1:4]])
     outputBlocks3 = tf.reshape(vectorizedOutput3,[batchSize,*blockShape[1:4]])
-    outputRows = concat_splits(value=outputBlocks,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
-    outputRows2 = concat_splits(value=replacementVals,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
-    outputRows3 = concat_splits(value=outputBlocks3,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
-    output = concat_splits(value=outputRows,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
-    output2 = concat_splits(value=outputRows2,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
-    output3 = concat_splits(value=outputRows3,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
+    output = ungather_blocks(outputBlocks,(pooledShape[1],pooledShape[2]))
+    output3 = ungather_blocks(outputBlocks3,(pooledShape[1],pooledShape[2]))
+    output2 = ungather_blocks(replacementVals,(pooledShape[1],pooledShape[2]))
+    #outputRows = concat_splits(value=outputBlocks,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
+    #outputRows2 = concat_splits(value=replacementVals,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
+    #outputRows3 = concat_splits(value=outputBlocks3,numOfSplits=pooledShape[2],splitDim=3,concatDim=2)
+    #output = concat_splits(value=outputRows,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
+    #output2 = concat_splits(value=outputRows2,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
+    #output3 = concat_splits(value=outputRows3,numOfSplits=pooledShape[1],splitDim=3,concatDim=1)
     return (output,output2,output3)
     
     
@@ -228,9 +257,11 @@ class multilayerADMMsparseCodingTightFrame(Layer):
       weightShape = tf.constant(value=[self.kerSz[ii][0],self.kerSz[ii][1],nof,self.noc[ii]])
       self.weights.append(self.add_weight(name=weightNames,
         shape=[self.kerSz[ii][0],self.kerSz[ii][1],nof,self.noc[ii]],#weightShape,
-        initializer=tf.keras.initializers.RandomNormal(mean=0.0,stddev=np.sqrt(2./(self.kerSz[ii][0]*self.kerSz[ii][1]*self.noc[ii])))))
+        initializer=tf.keras.initializers.RandomNormal(mean=0.0,stddev=np.sqrt(np.sqrt(1./(nof*self.kerSz[ii][0]**2*self.kerSz[ii][1]**2*self.noc[ii])))),
         #initializer=tf.keras.initializers.Orthogonal(gain=1.0),
-        #constraint=tf.keras.constraints.UnitNorm(axis=2)))
+        #constraint=tf.keras.constraints.UnitNorm(axis=3)
+        #constraint=tf.keras.constraints.MaxNorm(max_value=1.,axis=3)
+        ))
       nof = self.noc[ii]
     self.nof = inputShape[3]
     super(multilayerADMMsparseCodingTightFrame, self).build(input_shape)
@@ -265,14 +296,14 @@ class multilayerADMMsparseCodingTightFrame(Layer):
     z = []
     gamma = []
     mu = []
-    alpha.append(1./2.*dh(y,0))
+    alpha.append(dh(y,0))
     z.append(alpha[0])
     gamma.append(z[0] - alpha[0])
     mu.append(y - d(alpha[0],0))
 
     for ii in range(1,self.nol):
       poolz = max_pool(z[ii - 1],ii - 1)
-      alpha.append(1./2.*dh(poolz,ii))
+      alpha.append(dh(poolz,ii))
       z.append(alpha[ii])
       gamma.append(alpha[ii] - z[ii])
       mu.append(poolz - d(alpha[ii],ii))
@@ -315,3 +346,8 @@ class multilayerADMMsparseCodingTightFrame(Layer):
 
   def compute_output_shape(self, input_shape):
     return ([input_shape[0],self.outputSz[0],self.outputSz[1],self.noc[-1]],input_shape)
+
+
+#def multilayerADMMVisualizationNetwork(nol,model,inputShape)
+#  inputLayer = tensorflow.keras.Input(inputShape)
+#  return visualizationModel
