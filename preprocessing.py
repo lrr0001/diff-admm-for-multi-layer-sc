7f37bbe164892cd8c44aa03bc03523c290f10e3a
LRR <lrichert3@gatech.edu>
2019-09-13 17:54:23 -0400

    We have black and white CIFAR-10! Currently working on getting the local contrast normalization working so that I can inspect the output.

--- preprocessing.py
+++ preprocessing.py
@@ -2,30 +2,30 @@ import tensorflow
 import tensorflow.math
 from tensorflow.keras.layers import Layer
 from tensorflow import math
-import tensorflow.nn.conv2d
+import tensorflow.nn
 import numpy
 
 
-class local_contrast_normalization(Layer):
+class Local_contrast_normalization(Layer):
 
   def __init__(self,localSz,thrshld,**kwargs):
     self.localSz = localSz
     self.thrshld = thrshld
-    super(local_contrast_normalization, self).__init__(**kwargs)
+    super(Local_contrast_normalization, self).__init__(**kwargs)
 
   def build(self, input_shape):
-    super(local_constrast_normalization, self).build(input_shape)
+    super(Local_contrast_normalization, self).build(input_shape)
 
   def call(self, x):
     sigmah = self.localSz/6.0
-    gaussWeights = numpy.zeros((self.localSz,self.localSz,1,1))
+    gaussWeights = numpy.zeros((self.localSz,self.localSz,1,1),numpy.float32)
     for ii in range(self.localSz):
       for jj in range(self.localSz):
-        gaussWeights[ii,jj,0,0] = nump.exp(-((ii - self.localSz/2.0)^2 + (jj - self.localSz/2.0)^2)/(2.0*sigmah))
+        gaussWeights[ii,jj,0,0] = numpy.exp(-((ii - self.localSz/2.0)**2 + (jj - self.localSz/2.0)**2)/(2.0*sigmah))
     gaussWeights = gaussWeights/numpy.sum(gaussWeights)
     localMean = tensorflow.nn.conv2d(input=x,filter=tensorflow.convert_to_tensor(gaussWeights),strides=None,padding="SAME")
     demeanedX = x - localMean
-    normalizationFactor = tensorflow.nn.conv2d(input=tensorflow.math.square(demeanedX),filter=tensorflow.convert_to_tesnor(gaussWieghts),strides=None,padding="SAME")
+    normalizationFactor = tensorflow.nn.conv2d(input=tensorflow.math.square(demeanedX),filter=tensorflow.convert_to_tensor(gaussWeights),strides=None,padding="SAME")
     return tensorflow.where(normalizationFactor>self.thrshld,demeanedX/normalizationFactor,demeanedX)
 
   def compute_output_shape(self,input_shape):
